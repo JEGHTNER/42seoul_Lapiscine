74b8abf92443ed251b98911e3512901af907b4df
jehelee <jeheon0717@naver.com>
2022-08-24 18:09:19 +0900

    fixing

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-grep -v '#' passwd|awk NR!=1|cut -d : -f 1|rev|sort -r|awk '{if (NR>='$FT_LINE1' && NR<='$FT_LINE2') print ($0) }'|tr '\n' ', '|sed 's/.$/./g'|tr -d '\n'
+cat /etc/passwd| grep -v '#'|awk NR!=1|cut -d : -f 1|rev|sort -r|awk '{if (NR>='$FT_LINE1' && NR<='$FT_LINE2') print ($0) }'|tr '\n' ', '|sed 's/.$/./g'|tr -d '\n'
